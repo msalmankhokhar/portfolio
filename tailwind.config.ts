34eea630ae8130c5d5c067f2a5656371c36ac239
msalmankhokhar <kjokhars@gmail.com>
2025-01-16 20:37:37 +0500

    changed color to green

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,14 +10,23 @@ export default {
   theme: {
     extend: {
       colors: {
+        // primary: {
+        //   200: '#e2ecfe',
+        //   300: '#B6D1FB',
+        //   400: '#88B2F9',
+        //   500: '#6099F7',
+        //   600: '#387FF5',
+        //   700: '#2D66C4',
+        //   800: '#224C93',
+        // },
         primary: {
-          200: 'rgb(226, 236, 254)',
-          300: '#B6D1FB',
-          400: '#88B2F9',
-          500: '#6099F7',
-          600: '#387FF5',
-          700: '#2D66C4',
-          800: '#224C93',
+          200: '#e4f8e2', // light green
+          300: '#bcecb6', // soft green
+          400: '#88dc88', // medium green
+          500: '#60c960', // standard green
+          600: '#38b538', // vibrant green
+          700: '#2c8f2c', // deeper green
+          800: '#226922', // dark green
         },
         secondary: {
           100: '#F5F7FA',
